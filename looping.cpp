c2b95236b30409a608b544f6c14703a2262de5ae
ZahwaNabila <zahwanabila434@gmail.com>
2026-03-30 20:33:04 +0700

    commit1

---
+++ looping.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <ctime>
+using namespace std;
+
+int main( ){
+    int x;
+
+    cout<<"PERULANGAN FOR"<<endl;
+    for(int i=0;i<=4;i++){
+        cout<<"Teknologi Informasi UMY"<<endl;
+}
+}
