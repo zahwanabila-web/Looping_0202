8d3fe396c8db77d6ffef59f149d56cbf8fc897f9
ZahwaNabila <zahwanabila434@gmail.com>
2026-03-30 20:39:39 +0700

    commit2

--- looping.cpp
+++ looping.cpp
@@ -9,4 +9,14 @@ int main( ){
     for(int i=0;i<=4;i++){
         cout<<"Teknologi Informasi UMY"<<endl;
 }
+cout<<endl;
+srand(time(0));
+
+cout<<"PERULANGAN WHILE"<<endl;
+x = 1 + rand()%10;
+
+while(x<=5){
+    cout<<"Bilangan acak = "<<x <<endl;
+    x = rand()%10;
+}
 }
