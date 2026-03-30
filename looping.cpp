d720d5076a8568ea1eb3fc8e1361a516758bff09
ZahwaNabila <zahwanabila434@gmail.com>
2026-03-30 21:02:24 +0700

    commit5

--- looping.cpp
+++ looping.cpp
@@ -40,4 +40,15 @@ using namespace std;
 int main(){
     float nilB,nilM,rerata;
     string status;
+    cout<<"Masukkan nilai Matematika = ";
+    cin>>nilM;
+    cout<<"Masukkan nilai Bahasa Inggris = ";
+    cin>>nilB;
+
+    rerata = (nilB+nilM)/2;
+
+    if (!(rerata < 60) )
+        status = "Tidak lulus";
+
+    cout<<"Status kelulusan = "<<status<<" ,dengan nilai rata-rata = "<<rerata<<endl;
 }
