7d37d9855cbdf4d0f8334c516941b4fe627d394e
ZahwaNabila <zahwanabila434@gmail.com>
2026-03-30 21:19:06 +0700

    commit7

--- looping.cpp
+++ looping.cpp
@@ -65,4 +65,11 @@ int main(){
     cin>>nilB;
 
     rerata = (nilB+nilM)/2;
+
+    if ((rerata >= 60) && (nilM >= 70))
+        status = "Lulus";
+    else
+        status = "Tidak lulus";
+
+    cout<<"Status kelulusan = "<<status<<" ,dengan nilai rata-rata = "<<rerata<<endl;
 }
